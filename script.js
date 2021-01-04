f7cb52886bc120037ee84e9b2b5cf4ea2a752fa3
Adeshina Ibrahim <debowale01@gmail.com>
2021-01-04 23:59:43 +0100

    fix little stuff

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
 //variables
-
+const nameSearchForm = document.querySelector('#name-search-form');
 
 //get countries when page loads
 document.addEventListener('DOMContentLoaded', getCountries);
-
 async function getCountries(){
   const resp = await fetch('https://restcountries.eu/rest/v2/all');
   const countries = await resp.json();
-  console.log(countries);
   showCountries(countries);
 
 }
 
 function showCountries(countries){
-  let output;
+  let output = '';
   countries.forEach(country => {
     //display countries flags and details in the UI
     output += `<div class="countries__flag">
@@ -31,6 +29,56 @@ function showCountries(countries){
   const countriesFlags = document.querySelector('.countries__flags');
   countriesFlags.innerHTML = output;
 }
+
+//search country by name
+nameSearchForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const input = document.querySelector('#name-search').value;
+  searchCountryByFullName(input);
+
+  // if(e.keyCode === 13 || e.which === 13){
+  //   // console.log(input);
+  // }
+
+  
+});
+
+async function searchCountryByFullName(countryName){
+
+  const resp = await fetch(`https://restcountries.eu/rest/v2/name/${countryName}?fullText=true`);
+  const country = await resp.json();
+  //display the single country
+  displaySingleCountry(country);
+}
+
+function displaySingleCountry(country){
+  const {name, flag, population, region, capital} = country;
+  let flagsContainer = document.querySelector('.countries__flags');
+  // console.log(flagsContainer);
+  if(flagsContainer !== null){
+    flagsContainer.innerHTML = "";
+  }
+  const div = document.createElement('div');
+  div.className = 'countries__flag';
+  div.innerHTML = `<img src="${flag}" class="countries__flag-image" alt="${name}">
+          <div class="countries__short-details">
+            <h3 class="countries__name">${name}</h3>
+            <ul class="countries__properties">
+              <li class="countries__property"><b>Population:</b> ${population}</li>
+              <li class="countries__property"><b>Region:</b> ${region}</li>
+              <li class="countries__property"><b>Capital:</b> ${capital}</li>
+            </ul>
+          </div>`;
+  // const output = `<div class="countries__flag">
+          
+  //       </div>`;
+  // document.body.innerHTML = output;
+  flagsContainer.appendChild(div);
+
+  console.log(country);
+
+}
+
 
 function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
